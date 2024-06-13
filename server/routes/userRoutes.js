ebe384bb87055f7debe40940770c9f7f951bd4a8
siddharth <siddharth@scaler.com>
2024-06-13 09:35:06 +0530

    commit 3

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
+const bcrypt = require('bcrypt');
 
 router.post('/register', async (req, res) => {
-
   try {
     const userExists = await User.findOne({ email: req.body.email });
 
@@ -14,6 +14,10 @@ router.post('/register', async (req, res) => {
       })
       return;
     }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+
     const newUser = await User(req.body);
     await newUser.save();
   
@@ -31,8 +35,34 @@ router.post('/register', async (req, res) => {
 
 })
 
-module.exports = router;
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      res.send({
+        success: false,
+        message: "User does not exist"
+      })
+    }
 
-// router.post('/login', async (req, res) => {
-  
-// })
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+    if (!validPassword) {
+      res.send({
+        success: false,
+        message: "Invalid password"
+      })
+      return;
+    }
+
+    res.send({
+      success: true,
+      message: "Logged in"
+    })
+
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+module.exports = router;
